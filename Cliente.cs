b5b326b76d07c6e722c53ec864531775b4369707
EliasSuez <48240167@est.ort.edu.ar>
2024-05-06 08:50:46 -0300

    Crear nueva consola y variables

---
+++ Cliente.cs
@@ -0,0 +1,35 @@
+
+class Cliente
+{
+    public int DNI { get; private set; }
+    public int TipoEntrada { get; set; }
+    public int Cantidad { get; set; }
+    public string Apellido { get; private set; }
+    public string Nombre { get; private set; }
+    public double FechaInscripcion { get; set; }
+  
+    public Cliente(int dni, int tipoEntrada, int cantidad, string apellido, string nombre, double fechaInscripcion)
+    {
+        DNI = dni;
+        TipoEntrada = tipoEntrada;
+        Cantidad = cantidad;
+        Apellido = apellido;
+        Nombre = nombre;
+        FechaInscripcion = fechaInscripcion;
+    }
+
+
+    public static int Dinero(int TipoEntrada, int cantidad)
+    {
+        int dinerGastado;
+        
+        for (int i = 1; i < 4; i++)
+        {
+            if(TipoEntrada == i)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+int dni = 0, tipoEntrada = 0, cantidad = 0;
+string apellido = "", nombre = "";
+double fechaInscripcion = 0; 
+
+ValidarDatos(dni, tipoEntrada, cantidad, apellido, nombre, fechaInscripcion);
+
+Cliente cliente1 = new Cliente(dni, tipoEntrada, cantidad, apellido, nombre, fechaInscripcion);
+
+static string IngresarTexto(string mensaje)
+{
+    string ingreso;
+    Console.WriteLine(mensaje);
+    ingreso = Console.ReadLine();
+
+    return ingreso;
+}
+static int IngresarNumero(string mensaje)
+{
+    int ingreso;
+    Console.WriteLine(mensaje);
+    ingreso = int.Parse(Console.ReadLine());
+    return ingreso;
+}
+static double IngresarNumeroD(string mensaje)
+{
+    double ingreso;
+    Console.WriteLine(mensaje);
+    ingreso = int.Parse(Console.ReadLine());
+    return ingreso;
+}
+
+static void ValidarDatos(int dni,int tipoEntrada,int cantidad, string apellido,string nombre, double fechaInscripcion)
+{
+do{
+dni = IngresarNumero("Ingrese el DNI");
+}while(dni < 0);
+do{
+tipoEntrada = IngresarNumero("Ingrese el tipo de entrada, 1, 2, 3 o 4");
+}while(tipoEntrada < 0 || tipoEntrada > 4);
+do
+{
+cantidad = IngresarNumero("Ingrese la cantidad de entradas");
+}while(cantidad < 0);
+do
+{
+apellido = IngresarTexto("Ingrese el apellido");
+}while(apellido != " ");
+do
+{
+nombre = IngresarTexto("Ingrese el nombre");
+}while(apellido != " ");
+do
+{
+fechaInscripcion = IngresarNumeroD("Ingrese la cantidad de entradas");
+}while(cantidad < 0);
+}
